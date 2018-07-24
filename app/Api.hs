056ef1b30c83168c26210719fb73d3d7a32233d6
Neil de Carteret <n3dst4@gmail.com>
2018-07-24 11:15:00 +0100

    latest

---
+++ app/Api.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Data.Aeson   (FromJSON)
+import           Data.Monoid  (mconcat)
+import           GHC.Generics (Generic)
+import           Lib          (Alphabet, getAnagrams, getSubgrams, mkTree)
+import           Web.Scotty   (ActionM (..), get, html, jsonData, post, scotty)
+
+newtype Request = Request { haystack :: String } deriving (Generic, Show)
+
+instance FromJSON Request
+
+
+main = scotty 3000 $ do
+  get "/" $
+    html "<h1>post {haystack: \"haystack\"} to /subgrams to get subgrams</h1>"
+  get "/subgrams" $ do
+    r <- (jsonData :: ActionM Request)
+    html "<h1>not implemented</h1>"
--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,13 @@
-module Main where
+module Main (main) where
 
-import           Control.Monad (forever)
-import           Data.Char     (isLower)
-import           Lib           (Alphabet, getAnagrams, getSubgrams, mkTree)
-import           System.IO     (hFlush, stdout)
+import           Control.Monad         (forever)
+import           Data.Char             (isLower)
+import           Lib                   (Alphabet, getAnagrams, getSubgrams,
+                                        mkTree)
+import           System.Console.GetOpt (ArgDescr (NoArg), ArgOrder (Permute),
+                                        OptDescr (Option), getOpt)
+import           System.Environment    (getArgs)
+import           System.IO             (hFlush, stdout)
 
 aToZ :: Alphabet
 aToZ = ['a' .. 'z']
@@ -14,19 +18,32 @@ goodWord = do
   lc <- isLower . head
   return (len && lc)
 
+-- cmd line option flags
+data Flag = SubgramFlag | AnagramFlag deriving (Eq, Show)
+
+-- option definitions
+options :: [OptDescr Flag]
+options =
+  [ Option ['s'] ["subgram"] (NoArg SubgramFlag) "return subgrams instead of anagrams"
+  , Option ['a'] ["anagram"] (NoArg AnagramFlag) "return anagrams (default)"
+  ]
+
 main :: IO ()
 main = do
   text <- readFile "/usr/share/dict/words"
   let
     words = filter goodWord . lines $ text
     tree = mkTree aToZ words
-  putStrLn $ "Finished reading dictionary (" ++ (show $ length words) ++ ")"
+  putStrLn $ "Finished reading dictionary (" ++ show (length words) ++ ")"
   hFlush stdout
-  forever $ do
-    putStr "Enter your string: "
-    hFlush stdout
-    input <- getLine
-    let anagrams = getSubgrams aToZ input tree
-    putStrLn "Subgrams: "
-    print anagrams
-    hFlush stdout
+  args <- getArgs
+  let (opts, nonOpts, _) = getOpt Permute options args
+      -- i know we only have these two but this is an exercise in futureproofing
+      modeOpts = filter (`elem` [SubgramFlag, AnagramFlag]) opts
+      (action, description) = case reverse modeOpts of
+        SubgramFlag : _ -> (getSubgrams, "Subgrams")
+        _               -> (getAnagrams, "Anagrams")
+      text = concat nonOpts
+      results = action aToZ text tree
+  traverse putStrLn results
+  hFlush stdout
