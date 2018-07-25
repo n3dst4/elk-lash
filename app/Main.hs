4638314e66f069277ef87eed5a806a8ababbeeb4
Neil de Carteret <n3dst4@gmail.com>
2018-07-25 10:56:34 +0100

    get words from the onlines

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
-import           Control.Monad         (forever)
-import           Data.Char             (isLower)
-import           Lib                   (Alphabet, getAnagrams, getSubgrams,
-                                        mkTree)
-import           System.Console.GetOpt (ArgDescr (NoArg), ArgOrder (Permute),
-                                        OptDescr (Option), getOpt)
-import           System.Environment    (getArgs)
-import           System.IO             (hFlush, stdout)
-
+import           Control.Lens               ((^.))
+import           Control.Monad              (forever)
+import           Data.ByteString.Lazy.Char8 (unpack)
+import           Data.Char                  (isLower)
+import           Lib                        (Alphabet, getAnagrams, getSubgrams,
+                                             mkTree)
+import qualified Network.Wreq               as Wr
+import           System.Console.GetOpt      (ArgDescr (NoArg),
+                                             ArgOrder (Permute),
+                                             OptDescr (Option), getOpt)
+import           System.Environment         (getArgs)
+import           System.IO                  (hFlush, stdout)
 aToZ :: Alphabet
 aToZ = ['a' .. 'z']
+
+dictUrl :: String
+dictUrl = "https://gist.githubusercontent.com/n3dst4/ac7ca7fa851a396e4269d68aaaa6ed58/raw/b1f64ca00c94a4265d66d91940a658bd705e556f/words"
 
 goodWord :: String -> Bool
 goodWord = do
@@ -30,8 +37,11 @@ options =
 
 main :: IO ()
 main = do
-  text <- readFile "/usr/share/dict/words"
+  --text <- readFile "/usr/share/dict/words"
+  r <- Wr.get dictUrl
   let
+    textBS = r ^. Wr.responseBody
+    text = unpack textBS
     words = filter goodWord . lines $ text
     tree = mkTree aToZ words
   putStrLn $ "Finished reading dictionary (" ++ show (length words) ++ ")"
